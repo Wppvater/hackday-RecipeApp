a7d253e09e53d173bab3ddc1e5c86d31630cc0b6
Alexander Einerskog <77435792+Wppvater@users.noreply.github.com>
2021-07-02 15:10:39 +0200

    Configured

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,40 @@
 import logo from './logo.svg';
 import './App.css';
+import RecipeCreationForm from './components/forms/RecipeCreationForm';
+import RecipeSelectionForm from './components/forms/RecipeSelectionForm';
+import GroceryList from './components/GroceryList';
+import NavBar from './components/Navbar'
+import React, { useEffect, useState } from 'react';
+import {Route, Switch} from 'react-router-dom';
+const axios = require('axios');
 
-function App() {
+const App = () => {
+  const [ingredients, setIngredients] = useState(null);
+  const [recipes, setRecipes] = useState(null);
+  const [groceryList, setGroceryList] = useState(null);
+  const [addedNew, setAddedNew] = useState(null);
+  useEffect(()=>{
+    axios.get('http://localhost:9000/api/ingredients/names')
+      .then(data => setIngredients(data.data));
+    axios.get('http://localhost:9000/api/recipes/names')
+      .then(data => setRecipes(data.data));
+  }, [addedNew])
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <NavBar />
       </header>
+      <Switch>
+        <Route path='/recipes'>
+          <RecipeCreationForm ingredients={ingredients} addedNew={setAddedNew}/>
+        </Route>
+        <Route path='/groceries'>
+          <GroceryList groceries={groceryList}/>
+        </Route>
+        <Route path='/'>
+          <RecipeSelectionForm recipes={recipes} setGroceryList={setGroceryList}/>
+        </Route>
+      </Switch>
     </div>
   );
 }
